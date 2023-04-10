4c2f65061671b961506298fc9239058c4c5921a4
Muhammad Mominul Huque <mominul2082@gmail.com>
2023-04-11 03:37:22 +0600

    Login, logout implemented

--- profile/views.py
+++ profile/views.py
@@ -1,43 +1,55 @@
 from django.shortcuts import redirect, render, HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 def login_page(request):
+    if not request.user.is_authenticated:      
+        if request.method == 'POST':
+            email = request.POST['email']
+            password = request.POST['psw']
+            print(email, password)
+
+            user = authenticate(email=email, password=password)
+            print(user)
+
+            if User.objects.filter(email=email):
+                user = User.objects.get(email=email)
+                if user.check_password(password):
+                    login(request, user)
+                    print("LogIn Successfull")
+                    return redirect('/')
+            else:
+                print("username or password maybe incorrect")
+                return redirect('/login')
+
+        return render(request, "login.html")
     return render(request, 'login.html')
 def logout_page(request):
+    if request.user.is_authenticated:
+        logout(request)
     return render(request, 'logout.html')
 
 def signup_view(request):
     if not request.user.is_authenticated:
         if request.method == "POST":
-            username = request.POST['username']
+            fname = request.POST['fname']
+            lname = request.POST['lname']
+            username = fname + '_' + lname
             email = request.POST['email']
             password1 = request.POST['psw']
             password2 = request.POST['psw-repeat']
             
-            if User.objects.filter(username=username):
-                #messages.error(request, "Username already exist! please try some other username")
-                print("Username already exist! please try some other username")
-                return redirect('/signup')
-            
             if User.objects.filter(email=email):
-                # messages.error(request, "Emali already registered!")
                 print("Emali already registered!")
                 return redirect('/signup')
                         
             if password1 != password2:
-                # messages.error(request,"Password didn't match!")
                 print("Password didn't match!")
                 return redirect('/signup')
-            
-            if not username.isalnum():
-                # messages.error(request,"Username must be Alpha-Numeric!")
-                print("Username must be Alpha-Numeric!")
-                return redirect('/signup')
-
 
             newuser = User.objects.create_user(username, email, password1)
-            # newuser.is_active=False
             newuser.save()
+            return redirect('/')
     return render(request, "signup.html")
 
 def profile_view(request):
