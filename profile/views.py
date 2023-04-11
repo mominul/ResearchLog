1348cb95f757613a82621cfc26a6027358ace57b
Muhammad Mominul Huque <mominul2082@gmail.com>
2023-04-11 10:55:09 +0600

    Profile db connection

--- profile/views.py
+++ profile/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import redirect, render, HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from profile.models import Profile
 
 def login_page(request):
     if not request.user.is_authenticated:      
         if request.method == 'POST':
             email = request.POST['email']
             password = request.POST['psw']
-            print(email, password)
-
-            user = authenticate(email=email, password=password)
-            print(user)
 
             if User.objects.filter(email=email):
                 user = User.objects.get(email=email)
@@ -47,10 +44,57 @@ def signup_view(request):
                 print("Password didn't match!")
                 return redirect('/signup')
 
-            newuser = User.objects.create_user(username, email, password1)
+            newuser = User.objects.create_user(username, email, password1, first_name=fname, last_name=lname)
             newuser.save()
             return redirect('/')
     return render(request, "signup.html")
 
 def profile_view(request):
-    return render(request, "profile.html")
+    data = {}
+    if request.user.is_authenticated:      
+        if request.method == 'POST':
+            fname = request.POST['fname']
+            lname = request.POST['lname']
+            desc = request.POST['desc']
+            scholar_id = request.POST['scholar_id']
+            gh_id = request.POST['gh_id']
+
+            profile = None
+
+            if Profile.objects.filter(user_id= request.user.id):
+                profile = Profile.objects.get(user_id= request.user.id)
+            else:
+                profile = Profile(user_id = request.user.id)
+            
+            user = request.user
+            user.first_name = fname
+            user.last_name = lname
+            profile.description = desc
+            profile.gh_id = gh_id
+            profile.scholar_id = scholar_id
+            profile.save()
+            user.save()
+            print("Saved!")
+        else:
+            profile = None
+            user = User.objects.get(id=request.user.id)
+            print(user)
+
+            if Profile.objects.filter(user= request.user.pk):
+                profile = Profile.objects.get(user= request.user)
+            else:
+                profile = Profile()
+                print("not found")
+
+            data = {
+                'name': user.first_name + ' ' + user.last_name,
+                'fname': user.first_name,
+                'lname': user.last_name,
+                'desc': profile.description,
+                'scholar_id': profile.scholar_id,
+                'gh_id': profile.gh_id,
+            }
+            print(data)
+
+
+    return render(request, "profile_user.html", data)
